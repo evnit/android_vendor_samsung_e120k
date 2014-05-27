e854d392e86589557b9c9768d693f0be2854d75e
evnit <evnithn@gmail.com>
2014-05-27 21:24:56 +0700

    Update audio ...

--- e120k-vendor-blobs.mk
+++ e120k-vendor-blobs.mk
@@ -84,6 +84,7 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/e120k/proprietary/lib/hw/sensors.MSM8660_SURF.so:system/lib/hw/sensors.MSM8660_SURF.so \
 	vendor/samsung/e120k/proprietary/lib/libakm.so:system/lib/libakm.so \
 	vendor/samsung/e120k/proprietary/lib/hw/camera.msm8660.so:system/lib/hw/camera.msm8660.so \
+	vendor/samsung/e120k/proprietary/lib/hw/camera.vendor.msm8660.so:system/lib/hw/camera.vendor.msm8660.so \
 	vendor/samsung/e120k/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
 	vendor/samsung/e120k/proprietary/lib/libgemini.so:system/lib/libgemini.so \
 	vendor/samsung/e120k/proprietary/lib/libs3cjpeg.so:system/lib/libs3cjpeg.so \
